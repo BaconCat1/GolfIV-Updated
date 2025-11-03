abda1f207fde85c50439bb12081a106a1b092d00
BaconCat__ <126538523+BaconCat1@users.noreply.github.com>
2025-11-03 11:32:16 +0800

    Simplify Noconnectiondeath

--- src/main/java/org/samo_lego/golfiv/mixin/duplication/ServerPlayerEntityMixin_NoConnectionDeath.java
+++ src/main/java/org/samo_lego/golfiv/mixin/duplication/ServerPlayerEntityMixin_NoConnectionDeath.java
@@ -1,7 +1,5 @@
 package org.samo_lego.golfiv.mixin.duplication;
 
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -26,9 +24,13 @@ public abstract class ServerPlayerEntityMixin_NoConnectionDeath {
     /**
      * Checks whether player is even connected before applying damage.
      */
-    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
-    private void damage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if(golfConfig.duplication.patchDeathDuplication && this.isDisconnected())
+    @Inject(
+            method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void damage(CallbackInfoReturnable<Boolean> cir) {
+        if (golfConfig.duplication.patchDeathDuplication && this.isDisconnected())
             cir.setReturnValue(false);
     }
 }
